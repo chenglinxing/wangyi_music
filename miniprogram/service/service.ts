53f1e2e8da00d00488b5e4f7c3f3392c35059674
Tiamohub <1428054498@qq.com>
2023-07-24 13:45:52 +0800

    切换服务器地址

--- miniprogram/service/service.ts
+++ miniprogram/service/service.ts
@@ -2,7 +2,7 @@ export const service = (params: any) => {
   return new Promise((resolve, reject) => {
     wx.showLoading({title:"加载中"})
     wx.request({
-      url: 'http://60.205.95.118:3000' + params.requestUrl,
+      url: 'http://106.14.75.7:3000' + params.requestUrl,
       ...params,
       success(res) {
         resolve(res.data)
