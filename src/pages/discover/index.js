5463824c338122a170562cc8b585335e183fff58
chenglx <1428054498@qq.com>
2024-03-19 18:56:06 +0800

    240319 添加axios封装网络请求

--- src/pages/discover/index.js
+++ src/pages/discover/index.js
@@ -1,28 +1,39 @@
 import React, { memo } from "react";
-import { NavLink } from "react-router-dom"
-import { DiscoverWrapper, TopMenu} from "./style"
+import { NavLink } from "react-router-dom";
+import { DiscoverWrapper, TopMenu } from "./style";
 import { dicoverMenu } from "@/common/local-data";
 import { renderRoutes } from "react-router-config";
 
+import request from "@/services/request";
+import { useEffect } from "react";
+
 const Discover = memo((props) => {
-  const { route } = props
-  console.log(route)
-  return <DiscoverWrapper>
-  <div className="top">
-    <TopMenu className="wrap-v1">
-      {
-        dicoverMenu.map((item, index) => {
-          return (
-            <div className="item" key={item.title}>
-              <NavLink to={item.link}>{item.title}</NavLink>
-            </div>
-          )
-        })
-      }
-    </TopMenu>
-  </div>
-  {renderRoutes(route.routes)}
-</DiscoverWrapper>
+  useEffect(() => {
+    request({
+      url: "/banner",
+    }).then(res => {
+      console.log(res)
+    })
+  }, []);
+
+  const { route } = props;
+  console.log(route);
+  return (
+    <DiscoverWrapper>
+      <div className="top">
+        <TopMenu className="wrap-v1">
+          {dicoverMenu.map((item, index) => {
+            return (
+              <div className="item" key={item.title}>
+                <NavLink to={item.link}>{item.title}</NavLink>
+              </div>
+            );
+          })}
+        </TopMenu>
+      </div>
+      {renderRoutes(route.routes)}
+    </DiscoverWrapper>
+  );
 });
 
 export default Discover;
